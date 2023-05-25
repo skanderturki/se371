2cbe167dce8eb395d409f77b254457438c1ba893
skanderturki <skanderturki@gmail.com>
2023-05-25 19:37:24 +0300

    corrected some examples

--- final review/05-node-server/app.js
+++ final review/05-node-server/app.js
@@ -7,14 +7,14 @@ const itemsController = require('./controllers/itemsController');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); // for POST requests data
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', itemsController.getItems);
 app.post('/items/addItem', itemsController.addItem);
 
-mongoose.set('strictQuery', false);
+
 mongoose.connect(process.env.MONGO_URI)
   .then( (result) => {
     console.log("Connected to database...");
