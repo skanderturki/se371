253086df6bc88f542703bca6d4e61664b125e98e
--unset <--unset>
2023-10-10 08:08:21 +0300

    modified an example in chapter 4

--- chapter04/05_callback/map-reduce.js
+++ chapter04/05_callback/map-reduce.js
@@ -1,21 +1,30 @@
 
-let sum = (arr) => { 
+let sum = function(arr) { 
   let s = 0; 
   for (let e of arr) { 
-    s += e 
+    s += e; 
   }; 
-  return s 
+  return s;
 };
 
-let map = (f, arr) => { 
+let product = function(arr) { 
+  let s = 1; 
+  for (let e of arr) { 
+    s *= e; 
+  }; 
+  return s;
+};
+
+let map = function(f, arr) { 
   let res = [], i = 0; 
   for (let e of arr) { 
     res[i] = f(e); 
     i++; 
   }; 
-  return res 
+  return res; 
 };
 
 let arr = [[2, 3], [4, 6, 7], [8, 2], [89, 12]];
 
 console.log(map(sum, arr));
+console.log(map(product, arr));
