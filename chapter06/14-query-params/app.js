cc33369ed5ef0c54b0434b08b139e91b5c3bb9cd
--unset <--unset>
2024-04-22 08:21:19 +0300

    added example query params 14 chapter 6

--- chapter06/14-query-params/app.js
+++ chapter06/14-query-params/app.js
@@ -8,7 +8,6 @@ app.get('/', (request, response) => {
   response.json({"id": request.query.id, "name": request.query.name});
 } );
 
-
 app.use( (request, response) => {
   response.status(404).send("<h1>404 Error</h1>");
 });
